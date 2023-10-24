ada1dafa94b2c75822fd8de3115257fb7635a5d8
Ali İhsan Yüksel <alihsanyuksel@gmail.com>
2023-10-24 22:23:47 +0300

    first commit react-bootstrap

---
+++ bootstrapreact1/src/App.js
@@ -0,0 +1,13 @@
+import ThemeSwitcher from "./ThemeSwitcher";
+
+function App() {
+  return (
+    <div className="App min-vh-100 d-flex justify-content-center align-items-center">
+      <div>
+        <ThemeSwitcher />
+      </div>
+    </div>
+  );
+}
+
+export default App;
